4b35134c1fc3fb698e7a32dc0682949f632bbd88
Paul Molano <paulmolanoo@gmail.com>
2021-10-12 12:07:40 +0200

    Pregunta presupuesto

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import Pregunta from "./Components/Pregunta";
+
 function App() {
   return (
     <div className="container">
       <header>
         <h1>Gasto semanal</h1>
+        <div className="contenido-principal contenido">
+          <Pregunta />
+        </div>
       </header>
     </div>
   );
